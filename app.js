aa1c1719daf18401477e5a44bc71330df2dced89
Xuanyi Chen <troychen@bu.edu>
2021-06-25 19:21:33 +0800

    Move favicon to public/images

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ global.appRoot = path.resolve(__dirname);
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'images','favicon.ico')))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
