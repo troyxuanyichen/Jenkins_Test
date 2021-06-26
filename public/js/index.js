44ce0469edfe57d22adc13b4e3ac3b4640138c9c
Xuanyi Chen <troychen@bu.edu>
2021-06-26 19:32:39 +0800

    Get connect user name from URL

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,22 @@
-const socket = io();
-// const form = document.getElementById('form');
-// const nickname = document.getElementById('nickname');
-// const input = document.getElementById('input');
+const socket = io(); //connect
+console.log('Socket init')
 
+// Get username and room from URL
+const url = new URL(window.location);
+const username = url.searchParams.get('username');
 
-nickname.addEventListener('click', (e) => {
-    if (nickname.value) {
-        socket.emit('connect', nickname.value);
-    } else {
-        alert('Please choose a nick name');
-    }
-})
-input.addEventListener('click', (e) => {
-    if (!nickname.value) {
-        alert('Please choose a nick name');
-    } else if (input.value) {
-        socket.emit('chat message', input.value);
-        input.value = '';
-    }
-})
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (nickname.value && input.value) {
-        socket.emit('chat message', input.value);
-        input.value = '';
-    }
-}); //cannot add empty line here
+socket.emit('joinChat', username);
 
-socket.on('chat message', (msg) => {
+socket.on('message', (msg) => {
     const item = document.createElement('li');
     item.textContent = msg;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
-});
+})
+
+// socket.on('chat message', (msg) => {
+//     const item = document.createElement('li');
+//     item.textContent = msg;
+//     messages.appendChild(item);
+//     window.scrollTo(0, document.body.scrollHeight);
+// });
